1ac647a0187666562e278323fc9cda212aa4adf3
Afina <afinawalton95@gmail.com>
2021-12-15 14:41:54 -0800

    Passes event handler function to each component

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const App = () => {
         <button>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback={setSquares}/>
       </main>
     </div>
   );
