aff6029d2a3da7f7105e27b7d73fc811e628821f
Andrea Yuson <andreasyuson@gmail.com>
2021-12-17 14:21:08 -0800

    Tic Tac Toe

---
+++ src/Square.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Square.css';
+
+const Square = (props) => {
+  // For Wave 1 enable this
+  //  Component to alert a parent
+  //  component when it's clicked on.
+
+  return (
+    <button onClick={() => props.onClickCallback(props.id)} className="square">
+      {props.value}
+    </button>
+  );
+};
+
+Square.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClickCallback: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+export default Square;
+
+
+
+
+
+
+
+
