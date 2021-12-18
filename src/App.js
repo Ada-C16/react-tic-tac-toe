d6bdac8ba58b750aec6618ccc4c3ca112b3e9abc
goblinbrooke <BrookeVlahos@gmail.com>
2021-12-18 13:12:11 -0800

    I think everything is back? missing tests though

--- src/App.js
+++ src/App.js
@@ -21,21 +21,19 @@ const generateSquares = () => {
       currentId += 1;
     }
   }
+
   return squares;
 };
 
 const App = () => {
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
-  // Should have a board with 9 squares
-  // App -> Board -> [Square, Square, Square, Square, Square, Square, Square, Square, Square]
+
   const [squares, setSquares] = useState(generateSquares());
-  const [winner, setWinner] = useState(null);
+  const[winner, setWinner] = useState(null);
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
   const checkForWinner = () => {
     let i = 0;
-    // Check all the rows and columns for a winner
     while (i < 3) {
+      // Check all the rows and columns for a winner
       if (
         squares[i][0].value === squares[i][1].value &&
         squares[i][2].value === squares[i][1].value &&
@@ -68,9 +66,10 @@ const App = () => {
     ) {
       return squares[0][2].value;
     }
+  
     return null;
   };
-
+ 
   const onClickCallback = (id) => {
     setSquares((squares) => {
       let newBoard = squares.map((square)=>{
@@ -78,6 +77,7 @@ const App = () => {
           if (property.id === id && property.value === ''){
             if (currentPlayer === PLAYER_1){
               property.value = PLAYER_1;
+            
             } else if (currentPlayer === PLAYER_2){
               property.value = PLAYER_2;
             }
@@ -85,59 +85,45 @@ const App = () => {
         }
         return square;  
       });
-      // Check for winner
-      setWinner(checkForWinner());
-      return newBoard;
+      
+    //CHECK FOR WINNER HERE
+    setWinner(checkForWinner());
+
+    return newBoard;
     });
-    if (currentPlayer === PLAYER_1){
+    //CHANGE PLAYER
+    if (currentPlayer === PLAYER_1) {
       setCurrentPlayer(PLAYER_2);
-    } else {
-      setCurrentPlayer(PLAYER_1);
+    }else {
+      setCurrentPlayer(PLAYER_1)
     }
+    
   };
 
-  const onClickCallback = (id) => {
-    setSquares((squares) => {
-      let newBoard = squares.map((square)=>{
-        for (let property of square){
-          if (property.id === id && property.value === ''){
-            if (currentPlayer === PLAYER_1){
-              property.value = PLAYER_1;
-            } else if (currentPlayer === PLAYER_2){
-              property.value = PLAYER_2;
-            }
-          }
-        }
-        return square;
-      });
-      // Check for winner
-      return newBoard;
-    });
-    if (currentPlayer === PLAYER_1){
-      setCurrentPlayer(PLAYER_2);
-    } else {
-      setCurrentPlayer(PLAYER_1);
-    }
-  };
-
-
+  
 
   const resetGame = () => {
-    // Complete in Wave 4
     setSquares(generateSquares());
     setWinner(null);
     setCurrentPlayer(PLAYER_1);
   };
 
+  
   let header;
   let boardCallback;
+  // let finalWinner;
   if (winner != null) {
+    // if (winner === PLAYER_1) {
+    //   finalWinner = 'Player 1';
+    // } else {
+    //   finalWinner = 'Player 2';
+    // }
     header = <h2>Winner is {winner}</h2>;
   } else {
     header = <h2>The Current Player is {currentPlayer}</h2>;
     boardCallback = onClickCallback;
   }
-
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -153,6 +139,7 @@ const App = () => {
 };
 
 export default App;
+
 
   // Wave 2
   // You will need to create a method to change the square
