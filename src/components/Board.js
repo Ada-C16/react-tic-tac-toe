0d85dfc21e450666da1f0e1b6d84a1c135d2e1e4
Melinda <melindahayes84@gmail.com>
2021-12-18 15:20:57 -0800

    push changes test

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,31 +4,41 @@ import Square from './Square';
 import PropTypes from 'prop-types';
 
 
-const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
-  // squares is a 2D Array, but 
-  //  you need to return a 1D array
-  //  of square components
+const generateSquares = (squares, onClickCallback) => {
+  // by console logging an object shows the variable with variable name as the key
+  console.log({squares});
+  // need to unpack the squares prop that is coming in from app.js
+  const arrayToLoopThru = [];
+  const unpackedSquares = arrayToLoopThru.concat(...squares);
+  console.log({unpackedSquares});
 
-}
+  // how .map works From documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+return unpackedSquares.map(square => {
+const id = square.id
+const value = square.value
+  console.log({id, value});
+  // .map needs a key to return in the component https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md 
+  return <Square key={square.id}/>;
+  
+});
+
+  
+};
 
 const Board = ({ squares, onClickCallback }) => {
-  const squareList = generateSquareComponents(squares, onClickCallback);
+
+  const squareList = generateSquares(squares, onClickCallback);
   console.log(squareList);
   return <div className="grid" >
     {squareList}
-  </div>
-}
+  </div>;
+};
 
 Board.propTypes = {
-  squares: PropTypes.arrayOf(
-    PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        value: PropTypes.string.isRequired
-      })
-    )
-  ),
+  squares: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired
+  }))),
   onClickCallback: PropTypes.func.isRequired,
 };
 
