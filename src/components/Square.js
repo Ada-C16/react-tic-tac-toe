879bff76f6030ce4c5999d2fc5abaf549e2fb988
bailey <baileyholderman@gmail.com>
2021-12-20 15:23:46 -0800

    wave 1 walkthrough complete

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,10 +4,6 @@ import PropTypes from 'prop-types';
 import './Square.css';
 
 const Square = ({ value, id, onClickCallback }) => {
-  // For Wave 1 enable this
-  //  Component to alert a parent
-  //  component when it's clicked on.
-
   return (
     <button className="square" onClick={() => onClickCallback(id)}>
       {value}
