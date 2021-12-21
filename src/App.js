6790d53bc7da3d2fa6161e164c72f9cdc64ae7f8
Sophia Le <sophiale@sophias-mbp.lan>
2021-12-21 14:11:58 -0800

    Adds makeAnnouncement

--- src/App.js
+++ src/App.js
@@ -113,6 +113,16 @@ const App = () => {
     return 'Tied';
   };
 
+  const makeAnnouncement = (gameState) => {
+    if (gameState === 'Ongoing') {
+      return <h2>The game is ongoing.</h2>;
+    } else if (gameState === 'Tied') {
+      return <h2>The game is a tie.</h2>
+    } else {
+      return <h2>The winner is {gameState}.</h2>
+    }
+  };
+
   const onClickCallback = (id) => {
     const updatedSquares = [...squares];
     squares.forEach(row => {
