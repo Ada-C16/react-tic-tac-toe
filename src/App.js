df3cff30979fc7d7b144049cb53e5a797a230f33
Afina <afinawalton95@gmail.com>
2021-12-21 16:00:05 -0600

    Passes wave 3 and wave 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Board from './components/Board';
@@ -59,7 +59,11 @@ const App = () => {
     setSquares(newSquares);
   }
 
-  
+  // useEffect(checkForWinner, [squares])
+
+  // if (!checkForWinner()) {
+  //   ...
+  // }
 
   const checkForWinner = () => {
     // Complete in Wave 3
@@ -71,18 +75,73 @@ const App = () => {
     //    3 squares in each column match
     // 3. Go across each diagonal to see if
     //    all three squares have the same value.
+
+    // If there is no winner, display nothing
+    // If there is a winner, display the winner
+    // If there is a tie, y'all both lose!!!!!!!!
+
+    // This function will return x, o, tie, or none
+  
+    // [[{}, {}, {}], [{}, {}, {}], [{}, {}, {}]]
+
+    let squaresArray = [];
+
+    for (let rowIndex in squares) {
+      for (let objectIndex in squares[rowIndex]) {
+        squaresArray.push(squares[rowIndex][objectIndex].value)
+      }
+    }
+
+    // ROWS
+    if (squaresArray[0] != '' && squaresArray[0] === squaresArray[1] && squaresArray[0] === squaresArray[2]) {
+      return squaresArray[0]
+    } else if (squaresArray[3] != '' && squaresArray[3] === squaresArray[4] && squaresArray[3] === squaresArray[5]) {
+      return squaresArray[3]
+    } else if (squaresArray[6] != '' && squaresArray[6] === squaresArray[7] && squaresArray[6] === squaresArray[8]) {
+      return squaresArray[6]
+    } 
+    // COLUMNS 
+    else if (squaresArray[0] != '' && squaresArray[0] === squaresArray[3] && squaresArray[0] === squaresArray[6]) {
+      return squaresArray[0]
+    } else if (squaresArray[1] != '' && squaresArray[1] === squaresArray[4] && squaresArray[1] === squaresArray[7]) {
+      return squaresArray[1]
+    } else if (squaresArray[2] != '' && squaresArray[2] === squaresArray[5] && squaresArray[2] === squaresArray[8]) {
+      return squaresArray[2]
+    }
+    // DIAGONALS
+    else if (squaresArray[0] != '' && squaresArray[0] === squaresArray[4] && squaresArray[0] === squaresArray[8]) {
+      return squaresArray[0]
+    } else if (squaresArray[6] != '' && squaresArray[6] === squaresArray[4] && squaresArray[6] === squaresArray[2]) {
+      return squaresArray[6]
+    }
+
+    if (squaresArray.includes('')) {
+      return null
+    } else {
+      return "Tie"
+    }
   };
 
+  const declareWinner = () => {
+    if (!checkForWinner()) {
+      return <h2>It&apos;s Player {playerTurn}&apos;s turn!</h2>
+    } else if (checkForWinner() == 'Tie') {
+      return <h2>Y&apos;all both lose! &#128532;</h2>
+    } else {
+      return <h2>&#127881; Player {checkForWinner()} wins the game! &#127881;</h2>
+    }
+  }
+
   const resetGame = () => {
-    // Complete in Wave 4
+    setSquares(generateSquares());
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
-        <button>Reset Game</button>
+        {declareWinner()}
+        <button onClick={() => resetGame()}>Reset Game</button>
       </header>
       <main>
         <Board squares={squares} onClickCallback={changeSquareValue}/>
