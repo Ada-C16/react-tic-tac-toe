6a2ecb4b4b288752308c69907ce7f24920cb7cc3
Khandice <s.khandice@gmail.com>
2021-12-22 10:47:14 -0800

    Adds semicolons

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
   const toggleTurn = () => {
     setPlayerTurn(playerTurn === PLAYER_1 ? PLAYER_2 : PLAYER_1);
-  }
+  };
 
   const [squares, setSquares] = useState(generateSquares());
 
@@ -44,60 +44,60 @@ const App = () => {
           value: playerTurn});
           toggleTurn();
         } else 
-        {rowArray.push(column)}
+        {rowArray.push(column);}
       } newSquares.push(rowArray); 
     }
     setSquares(newSquares);
-  }
+  };
 
   const checkForWinner = () => {
     let squaresArray = [];
 
     for (let rowIndex in squares) {
       for (let objectIndex in squares[rowIndex]) {
-        squaresArray.push(squares[rowIndex][objectIndex].value)
+        squaresArray.push(squares[rowIndex][objectIndex].value);
       }
     }
 
     // ROWS
     if (squaresArray[0] != '' && squaresArray[0] === squaresArray[1] && squaresArray[0] === squaresArray[2]) {
-      return squaresArray[0]
+      return squaresArray[0];
     } else if (squaresArray[3] != '' && squaresArray[3] === squaresArray[4] && squaresArray[3] === squaresArray[5]) {
-      return squaresArray[3]
+      return squaresArray[3];
     } else if (squaresArray[6] != '' && squaresArray[6] === squaresArray[7] && squaresArray[6] === squaresArray[8]) {
-      return squaresArray[6]
+      return squaresArray[6];
     } 
     // COLUMNS 
     else if (squaresArray[0] != '' && squaresArray[0] === squaresArray[3] && squaresArray[0] === squaresArray[6]) {
-      return squaresArray[0]
+      return squaresArray[0];
     } else if (squaresArray[1] != '' && squaresArray[1] === squaresArray[4] && squaresArray[1] === squaresArray[7]) {
-      return squaresArray[1]
+      return squaresArray[1];
     } else if (squaresArray[2] != '' && squaresArray[2] === squaresArray[5] && squaresArray[2] === squaresArray[8]) {
-      return squaresArray[2]
+      return squaresArray[2];
     }
     // DIAGONALS
     else if (squaresArray[0] != '' && squaresArray[0] === squaresArray[4] && squaresArray[0] === squaresArray[8]) {
-      return squaresArray[0]
+      return squaresArray[0];
     } else if (squaresArray[6] != '' && squaresArray[6] === squaresArray[4] && squaresArray[6] === squaresArray[2]) {
-      return squaresArray[6]
+      return squaresArray[6];
     }
 
     if (squaresArray.includes('')) {
-      return null
+      return null;
     } else {
-      return "Tie"
+      return 'Tie';
     }
   };
 
   const declareWinner = () => {
     if (!checkForWinner()) {
-      return <h2>It&apos;s Player {playerTurn}&apos;s turn!</h2>
+      return <h2>It&apos;s Player {playerTurn}&apos;s turn!</h2>;
     } else if (checkForWinner() == 'Tie') {
-      return <h2>Y&apos;all both lose! &#128532;</h2>
+      return <h2>Y&apos;all both lose! &#128532;</h2>;
     } else {
-      return <h2>&#127881; Player {checkForWinner()} wins the game! &#127881;</h2>
+      return <h2>&#127881; Player {checkForWinner()} wins the game! &#127881;</h2>;
     }
-  }
+  };
 
   const resetGame = () => {
     setSquares(generateSquares());
