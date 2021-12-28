4ede68d160605752b2e5ef281d5aa232ee04cab5
Gabe Kelemen <gabe@gabekelemen.com>
2021-12-28 09:36:20 -0500

    adds event handler to update input fields and state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Login = () => {
+  const [formFields, setFormFields] = useState({
+    userName1: '',
+    userName2: '',
+  });
+
+  const onPlayer1NameChange = (event) => {
+    setFormFields({
+      ...formFields,
+      userName1: event.target.value,
+    });
+  };
+
+  const onPlayer2NameChange = (event) => {
+    setFormFields({
+      ...formFields,
+      userName2: event.target.value,
+    });
+  };
+
   return (
     <form>
       <div>
         <label htmlFor="player1">Player X name: </label>
-        <input type="text" id="player1" />
+        <input
+          userName1="player1Name"
+          value={formFields.userName1}
+          onChange={onPlayer1NameChange}
+          type="text"
+          id="player1"
+        />
       </div>
       <div>
         <label htmlFor="player2">Player O name: </label>
-        <input type="text" id="player2" />
+        <input
+          userName2="player2Name"
+          value={formFields.userName2}
+          onChange={onPlayer2NameChange}
+          type="text"
+          id="player2"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" value="Add Player Name">
+        Submit
+      </button>
     </form>
   );
 };
